7a4541e735b3a5bc4ecb7b703830ce4510740bc0
emeka nwobu <emecusxyz@yahoo.com>
2023-09-15 04:35:14 -0700

    simple shell

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdbool.h>
+
+#endif
+
